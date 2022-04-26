d59e98a89751e61f30d90667e25232301a55ee23
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-04-25 20:50:24 -0500

    Updates

---
+++ precache-manifest.127627736bdcaa2523dadc2029f98974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c6e4fcece50f8013327",
+    "url": "/portfolio-brenda/static/css/main.8457a3e7.chunk.css"
+  },
+  {
+    "revision": "7c6e4fcece50f8013327",
+    "url": "/portfolio-brenda/static/js/main.7c6e4fce.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/portfolio-brenda/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "413b728f6bda178a29fd",
+    "url": "/portfolio-brenda/static/js/2.413b728f.chunk.js"
+  },
+  {
+    "revision": "2737d72c82de5585bbd1",
+    "url": "/portfolio-brenda/static/js/runtime~main.2737d72c.js"
+  },
+  {
+    "revision": "aa3b999c0b10886148b84ffe5fec063d",
+    "url": "/portfolio-brenda/static/media/webdev.aa3b999c.jpeg"
+  },
+  {
+    "revision": "6e557bc769af0d58a295968b33fe5eb3",
+    "url": "/portfolio-brenda/index.html"
+  }
+];
