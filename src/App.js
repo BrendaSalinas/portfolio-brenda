e0cf13d4fce42975737f9dee3460cc77949ee605
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-04-25 19:27:39 -0500

    The first part of the landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import './App.css';
+import Main from "./components/main/Main.js";
+
 const App = () => {
-  return <div>Hellooooooo</div>;
+  return <div className='app-1'> <Main/> </div>;
 };
 
 export default App;
---
+++ src/components/main/main.js
@@ -0,0 +1,22 @@
+import './main.css'
+const Main = () => {
+    return (
+        <div className = 'main'>
+            <div className="main-left">
+                <div className='main-left-wrap'>
+                    <h3 className='introduction'> Hello, My name is</h3>
+                    <h2 className='name'>Brenda Salinas</h2>
+                    <div className='title'>
+                        <div className='title-wrap'>
+                            <div className='title-1'>Full Stack Web Developer</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="main-right">right</div>
+            
+        </div>
+    )
+}
+
+export default Main
